bc9c7188c45f334829854c6ded0fb6373ee00624
Sohva <33163737+Sohva@users.noreply.github.com>
2020-07-07 10:04:38 +0100

    Transpose solving

--- TensorPrototype/Solver.jl
+++ TensorPrototype/Solver.jl
@@ -3,7 +3,15 @@ include("Operations.jl")
 function solve(add::AddOperation)
     sum = fill(0, add.shape)
     for node in add.children
-        sum += node.value
+        sum += solve(node)
     end
     return sum
+end
+
+function solve(tensor::ConcreteTensor)
+    return tensor.value
+end
+
+function solve(trans::TransposeOperation)
+    return transpose(solve(trans.children[1]))
 end
