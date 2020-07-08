42172256c418b80ea91182ab1c8923bb2fbfb39c
Sohva <33163737+Sohva@users.noreply.github.com>
2020-07-08 12:45:31 +0100

    ID example

--- TensorPrototype/Scripts/StructEquality.jl
+++ TensorPrototype/Scripts/StructEquality.jl
@@ -6,13 +6,30 @@ mutable struct MutableX
     x
 end
 
+counter = 0
+
+struct CounterX
+    x
+    id
+    function CounterX(x)
+        global counter
+        counter += 1
+        new(x, counter)
+    end
+end
+
+
 a = ImmutableX(1)
 b = ImmutableX(1)
 a_mutable = MutableX(1)
 b_mutable = MutableX(1)
 a_array = ImmutableX([1])
 b_array = ImmutableX([1])
+a_counter = CounterX(1)
+b_counter = CounterX(1)
 
 println(a === b) # True
 println(a_mutable === b_mutable) #False
 println(a_array === b_array) # False
+println(a_counter === b_counter) # False
+println(a_counter == b_counter) # False
