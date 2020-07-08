5aa5bdd2809aa2801f84853997e8480836e5806c
Sohva <33163737+Sohva@users.noreply.github.com>
2020-07-08 17:04:41 +0100

    Fixed index creation function

--- TensorPrototype/Indices.jl
+++ TensorPrototype/Indices.jl
@@ -14,6 +14,12 @@ Base.adjoint(i::FreeIndex) = FreeIndex(dual(i.V))
 struct FixedIndex <: Index
     value::Int
     V::AbstractVectorSpace
+    function FixedIndex(value::Int, V::AbstractVectorSpace)
+        if value < 1 || value > dim(V)
+            error("Index not in range")
+        end
+        new(value, V)
+    end
 end
 
 Base.adjoint(i::FixedIndex) = FixedIndex(i.value, dual(i.V))
