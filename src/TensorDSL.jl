5bf09895f3ff3709d6797f247e89a022fbc2c088
Sohva <33163737+Sohva@users.noreply.github.com>
2020-08-04 13:53:18 +0200

    Added the rest

--- src/TensorDSL.jl
+++ src/TensorDSL.jl
@@ -1,10 +1,29 @@
 module TensorDSL
 
-# Write your package code here.
 include("TensorPrototype/Node.jl")
 include("TensorPrototype/VectorSpace.jl")
+include("TensorPrototype/Indices.jl")
+include("TensorPrototype/Tensors.jl")
+include("TensorPrototype/Operations.jl")
+include("TensorPrototype/Trigonometry.jl")
+include("TensorPrototype/Differentation.jl")
 
-export VectorSpace
+export
+Node,
+
+AbstractVectorSpace, VectorSpace, DualVectorSpace, RnSpace, dual, dim,
+
+Index, FreeIndex, FixedIndex, Indices, toindex,
+
+AbstractTensor, TerminalTensor, ScalarVariable, Scalar, VariableTensor,
+Tensor, DeltaTensor, ZeroTensor, ConstantTensor,
+
+Operation, IndexSumOperation, AddOperation, OuterProductOperation, ⊗,
+IndexingOperation, ComponentTensorOperation, componenttensor,
+
+SineOperation, sin, CosineOperation, cos, TangentOperation, tan,
+
+diff
+
 
 end
-
