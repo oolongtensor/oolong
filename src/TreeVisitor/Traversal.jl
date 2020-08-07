b39cb7d80915e66400a8665362017fba6566ed01
Sohva <33163737+Sohva@users.noreply.github.com>
2020-08-07 17:07:59 +0200

    Unified function calls

--- src/TreeVisitor/Traversal.jl
+++ src/TreeVisitor/Traversal.jl
@@ -14,7 +14,7 @@ end
 function traversal(node::Node, pretraversalfn::Function, visitfn::Function,
         pretraversalfnargs::Union{Any, Nothing}, visitfnargs::Union{Any, Nothing})
     root = RootNode(node)
-    root = pretraversalfnargs !== nothing ? pretraversalfn(root, pretraversalfnargs...) : pretraversalfn(root)
+    root = pretraversalfnargs !== nothing ? pretraversalfn(root, pretraversalfnargs) : pretraversalfn(root)
     root = _traversal(root, visitfn, visitfnargs)
     return root.children[1]
 end
