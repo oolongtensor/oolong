9fae971728539ad3e62d90c176fd872e2efb7de1
Sohva <33163737+Sohva@users.noreply.github.com>
2020-08-07 17:18:45 +0200

    Number tensor can be turned to Gem nodes

--- src/GemJL/ToGem.jl
+++ src/GemJL/ToGem.jl
@@ -2,6 +2,10 @@ function _togem(A::Tensor{T}) where T<: Number
     return LiteralGemTensor(A.value)
 end
 
+function _togem(root::RootNode, child::Vararg{Node})
+    return updatechildren(root, child...)
+end
+
 function togem(node::Node)
     return traversal(node, x-> x, _togem, nothing, nothing)
 end
--- src/TreeVisitor/Traversal.jl
+++ src/TreeVisitor/Traversal.jl
@@ -9,6 +9,14 @@ function _traversal(node::Node, visitfn::Function, visitfnargs::Union{Any, Nothi
     return visitfnargs !== nothing ? visitfn(node, visitfnargs, new_children...) : visitfn(node, new_children...)
 end
 
+```Visits every successor of node and returns the result.
+
+If visitfnargs, visitfn is called with node, visitfnargs, new_children... where
+new_children are the results of calling visitfn to the children of node.
+If visitfnargs === nothing, visitfn is called with node, new_children...
+
+pretraversalfn is called before traversal similarly to visitfn.
+```
 function traversal(node::Node, pretraversalfn::Function, visitfn::Function,
         pretraversalfnargs::Union{Any, Nothing}, visitfnargs::Union{Any, Nothing})
     root = RootNode(node)
