8683a2e5e297d1fe1579d496c8cac94e7d033d0a
Sohva <33163737+Sohva@users.noreply.github.com>
2020-08-18 18:21:36 +0100

    Reduced index sum

--- src/GemJL/BetaReduction.jl
+++ src/GemJL/BetaReduction.jl
@@ -1,8 +1,10 @@
-function _betareduction(node::Node)
-    return node
+function _betareduction(in::IndexSumGem, indices::pair{GemIndex, GemIndex}, A::ScalarGem)
+    i, j = indices
+    return in.index == i ? IndexSumGem(A, j) : IndexSumGem(A, in.index)
 end
 
-function _betareduction(in::IndexSumGem, indices::pair{GemIndex, GemIndex}, A::ScalarGem)
+function _betareduction(node::Node, indices::pair{GemIndex, GemIndex})
+    return node
 end
 
 ```Replaces a GemIndex in an expression with another GemIndex.
