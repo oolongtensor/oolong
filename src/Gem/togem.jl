c3b5247db6b15801cc113c151c629670c07dc85a
Sohva <sofia.h.e.simola@gmail.com>
2020-08-25 15:36:36 +0100

    Removed draft functions that won't be implemented

--- src/Gem/togem.jl
+++ src/Gem/togem.jl
@@ -71,6 +71,7 @@ function _togem(add::AddOperation, children::Vararg{PyObject})
     end
     return gem.ComponentTensor(sum, indices)
 end
+
 #=
 function _togem(ou::OuterProductOperation, A::ScalarGem, B::ScalarGem)
     return ProductGem(A, B)
@@ -94,54 +95,6 @@ end
 
 _count = 0
 
-```Takes a set of tensors and indices them by the same set of indices. Returns
-a list of the indexed tensors and the indices.
-```
-function _indextensors(tensors::Vararg{GemTensor{rank}}) where rank
-    global _count
-    indices = []
-    for i in 1:rank
-        push!(indices, GemIndex(shape(tensors[1])[i], "togem", _count))
-        _count += 1
-    end
-    return [IndexedGem(tensor, indices...) for tensor in tensors], indices
-end
-
-function _togem(add::AddOperation, children::Vararg{GemTensor{rank}}) where rank
-    indexed, indices = _indextensors(children...)
-    return ComponentTensorGem(SumGem(indexed...),  indices...)
-end
-
-function _togem(op::OuterProductOperation, A::ScalarGem, B::GemTensor{rank}) where rank
-    indexed, indices = _indextensors(B)
-    return ComponentTensorGem(ProductGem(A, indexed...), indices...)
-end
-
-function _togem(op::OuterProductOperation, A::GemTensor{rank}, B::ScalarGem) where rank
-    indexed, indices = _indextensors(A)
-    return ComponentTensorGem(ProductGem(indexed..., B), indices...)
-end
-
-function _togem(op::OuterProductOperation, A::GemTensor{rank}, B::GemTensor{rank}) where rank
-    indexedA, indicesA = _indextensors(A)
-    indexedB, indicesB = _indextensors(B)
-    return ComponentTensorGem(ProductGem(indexedA..., indexedB...),  indicesA..., indicesB...)
-end
-
-function _togem(comp::ComponentTensorOperation, A::ComponentTensorGem{rank},
-        indices::Tuple{Vararg{GemIndex}}) where rank
-    return ComponentTensorGem(A.children[1], tuple(union(A.children[1].indices, indices)...)...)
-end
-
-function _togem(is::IndexSumOperation, A::ComponentTensorGem{rank},
-        indices::Tuple{Vararg{GemIndex}}) where rank
-    indexed, componentindices = _indextensors(A)
-    A = indexed[1]
-    for i in indices
-        A = IndexSumGem(A, i)
-    end
-    return ComponentTensorGem(A, componentindices...)
-end
 =#
 
 function _togem(root::RootNode, node)
