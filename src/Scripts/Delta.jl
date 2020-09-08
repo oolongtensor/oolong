e9908fa5367979f45e4a4232e24966ad8e4dc01b
Sohva <sofia.h.e.simola@gmail.com>
2020-09-08 11:38:13 +0100

    Scartch for trasnslating generalised delta into numbers

--- src/Scripts/Delta.jl
+++ src/Scripts/Delta.jl
@@ -5,20 +5,18 @@ function sign(per1, per2)
     for i in 1:length(per1)
         if per1[i] != per2[i]
             changes += 1
-            println(i, per1,  per2)
             j = findfirst(x -> (x == per1[i]), per2[(i+1):end]) + i
             per2[j] = per2[i]
-            println(per2, j)
         end
     end
     return iseven(changes) ? 1 : -1
 end
 
-function getvalue(index, upper)
+function getvalue(index)
     upperindices = []
     lowerindices = []
     for i in 1:length(index)
-        if upper[i]
+        if isodd(i)
             push!(upperindices, index[i])
         else
             push!(lowerindices, index[i])
@@ -31,12 +29,11 @@ end
 function shapetodelta(Vs::Vararg{AbstractVectorSpace})
     # Check the shape is legal
     DeltaTensor(Vs...)
-    upper = [V isa DualVectorSpace for V in Vs]
     shape = [dim(V) for V in Vs]
     delta = Array{Int}(undef, shape...)
     indices = CartesianIndices(tuple(shape...))
     for index in indices
-        delta[index] = getvalue(index, upper)
+        delta[index] = getvalue(index)
     end
     return delta
 end
