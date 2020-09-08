7312e155bf1817449114897189554635e1e667c5
Sohva <sofia.h.e.simola@gmail.com>
2020-09-08 16:35:32 +0100

    Differentation uses the new traversal

--- src/TensorPrototype/Differentation.jl
+++ src/TensorPrototype/Differentation.jl
@@ -1,34 +1,38 @@
 """Differentiates A w.r.t y"""
-function differentiate(A::Union{Tensor{T, 0}, ConstantTensor{T, 0}}, y::VariableTensor{0}) where T
+function _differentiate(A::Union{Tensor{T, 0}, ConstantTensor{T, 0}}, y::VariableTensor{0}, difffn) where T
     return A.value == y || A.value == [y] ? ConstantTensor(1) : ZeroTensor()
 end
 
-function differentiate(A::VariableTensor{0}, y::VariableTensor{0})
+function _differentiate(A::VariableTensor{0}, y::VariableTensor{0}, difffn)
     return A == y ? ConstantTensor(1) : ZeroTensor()
 end
 
-"""Differentiates A w.r.t y"""
-function differentiate(Z::ZeroTensor{0}, y::VariableTensor{0})
+function _differentiate(Z::ZeroTensor{0}, y::VariableTensor{0}, difffn)
     return Z
 end
 
-"""Differentiates A w.r.t y"""
-function differentiate(add::AddOperation{0}, y::VariableTensor{0})
-    return +([differentiate(child, y) for child in add.children]...)
+function _differentiate(add::AddOperation{0}, y::VariableTensor{0}, difffn)
+    return +([difffn(child) for child in add.children]...)
+end
+
+function _differentiate(op::OuterProductOperation{0}, y::VariableTensor{0}, difffn)
+    A, B = op.children
+    return A*difffn(B) + difffn(A)*B
+end
+
+function _differentiate(si::SineOperation{0}, y::VariableTensor{0}, difffn)
+    return Base.cos(si.children[1]) * difffn(si.children[1])
+end
+
+function _differentiate(co::CosineOperation{0}, y::VariableTensor{0}, difffn)
+    return - Base.sin(co.children[1]) * difffn(co.children[1])
+end
+
+function _differentiate(root::RootNode, y::VariableTensor{0}, diffn)
+    return RootNode(diffn(root.children[1]))
 end
 
 """Differentiates A w.r.t y"""
-function differentiate(op::OuterProductOperation{0}, y::VariableTensor{0})
-    (A, B) = op.children
-    return A*differentiate(B, y) + differentiate(A, y)*B
-end
-
-"""Differentiates A w.r.t y"""
-function differentiate(si::SineOperation{0}, y::VariableTensor{0})
-    return Base.cos(si.children[1]) * differentiate(si.children[1], y)
-end
-
-"""Differentiates A w.r.t y"""
-function differentiate(co::CosineOperation{0}, y::VariableTensor{0})
-    return - Base.sin(co.children[1]) * differentiate(co.children[1], y)
+function differentiate(A::AbstractTensor{0}, x::VariableTensor{0})
+    return traversal(A, x -> x, _differentiate, nothing, x, nothing, true)
 end
