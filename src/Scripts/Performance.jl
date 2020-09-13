04084c31d431bd02a550bbd99ef7d2d12c2a505d
Sohva <sofia.h.e.simola@gmail.com>
2020-09-13 12:24:29 +0100

    Performance script

--- src/Scripts/Performance.jl
+++ src/Scripts/Performance.jl
@@ -1,6 +1,12 @@
 using TensorDSL
+using BenchmarkTools
+using PyCall
 
-dim = 30
+dim = 40
 V = VectorSpace(dim)
-A = Tensor(V, V')
+A = VariableTensor("A", V, V', V)
+x = FreeIndex(V, "x")
+knl = Kernel((4*A)[x, x'])
 
+b = @benchmarkable execute($knl, "A"=>val) setup = (val = rand(Float64, (dim, dim, dim))) evals = 1
+run(b, samples=100)
